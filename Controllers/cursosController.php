c795a994f707f98af6cb3fa595c933c043588a02
Dhanielt Felipe Rubio Prasca <dfrubio@unillanos.edu.co>
2024-05-30 02:29:48 -0500

    no se

--- Controllers/cursosController.php
+++ Controllers/cursosController.php
@@ -19,4 +19,25 @@ class cursosController {
             return $e->getMessage();
         }
     }
+    public function getCursos($cod_doc, $periodo, $año){
+        return $this->getCursos($cod_doc,$periodo, $año);
+    }
+
+    public function manejoSolicitud(){
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            // Obtener el cuerpo de la solicitud
+            $json = file_get_contents('php://input');
+            // Decodificar el JSON en un array asociativo
+            $notas = json_decode($json, true);
+            echo $notas;
+            // Verificar si se decodificó correctamente
+            $partes = explode("-", $notas);
+            if ($notas !== null) {
+                echo $this->getCursos($partes[2], $partes[1], $partes[0]);
+            } else {
+                // Error al decodificar JSON
+                echo "Error al decodificar JSON";
+            }
+        }
+    }
 }
--- Controllers/reporte.php
+++ Controllers/reporte.php
@@ -16,11 +16,19 @@ class ReporteControlador {
         return $this->modelo->getNotas($cod_inf);
     }
 
+    public function getDefNotas($año, $periodo, $cod_doc, $cod_cur, $corte){
+        $this->modelo->getDefNotas($año, $periodo, $cod_doc, $cod_cur, $corte);
+    }
+
     public function manejarPeticion() {
         if (isset($_GET['corte']) && $_GET['corte'] != '' && isset($_GET['cod_cur']) && $_GET['cod_cur'] != '') {
             header('Content-Type: application/json');
             echo json_encode($this->getInfoNotas($_GET['corte'], $_GET['cod_cur']));
         } else if(isset($_GET['cod_inf']) && $_GET['cod_inf'] != ''){
+            header('Content-Type: application/json');
+            echo json_encode($this->getNotas($_GET['cod_inf']));
+        }else { 
+            $this->modelo->getDefNotas($_GET['año'],$_GET['$periodo'], $_GET['$cod_doc'], $_GET['$cod_cur'], $_GET['$corte']);
             header('Content-Type: application/json');
             echo json_encode($this->getNotas($_GET['cod_inf']));
         }
